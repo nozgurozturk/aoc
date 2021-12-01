8398eebfb30d3bf32fe1d79890f1c4b7c4f899f9
n.ozgurozturk <ozgur@nozgurozturk.com>
2021-12-01 13:18:30 +0100

    solve 2021-01 [part-two]

--- 2021/01/solution.go
+++ 2021/01/solution.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"aoc/pkg/io"
+	"aoc/pkg/math"
 	"fmt"
+	"os"
 )
 
 const FileName = "INPUT"
@@ -10,18 +12,30 @@ const FileName = "INPUT"
 func main() {
 
 	depths := io.ReadIntegers(FileName)
+	windowSize := math.ParseInt(os.Args[1])
 
-	result := SonarSweep(depths)
+	result := SonarSweep(depths, windowSize)
 
 	fmt.Println(result)
 }
 
-func SonarSweep(depths []int) int {
+func SonarSweep(depths []int, windowSize int) int {
 	inc := 0
-	for i := 1; i < len(depths); i++ {
-		if depths[i] > depths[i-1] {
+	prevSum := 0
+
+	for i := 0; i <= len(depths)-windowSize; i++ {
+		window := make([]int, windowSize, windowSize)
+
+		for j := 0; j < windowSize; j++ {
+			window[j] = depths[i+j]
+		}
+
+		currentSum := math.SumOf(window...)
+		if currentSum > prevSum && prevSum > 0{
 			inc++
 		}
+
+		prevSum = currentSum
 	}
 	return inc
 }
--- 2021/01/solution_test.go
+++ 2021/01/solution_test.go
@@ -6,8 +6,9 @@ import (
 
 func TestSonarSweep(t *testing.T) {
 	tests := []struct {
-		input    []int
-		expected int
+		input      []int
+		windowSize int
+		expected   int
 	}{
 		{
 			[]int{
@@ -22,13 +23,29 @@ func TestSonarSweep(t *testing.T) {
 				260,
 				263,
 			},
+			1,
 			7,
+		},	{
+			[]int{
+				199,
+				200,
+				208,
+				210,
+				200,
+				207,
+				240,
+				269,
+				260,
+				263,
+			},
+			3,
+			5,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run("", func(t *testing.T) {
-			result := SonarSweep(test.input)
+			result := SonarSweep(test.input, test.windowSize)
 
 			if result != test.expected {
 				t.Errorf("expected %6d, result %6d\n", test.expected, result)
